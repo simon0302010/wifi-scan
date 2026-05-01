9379aad63e97561250e0f99ec11b696f059004d1
simon0302010 <simon@hackclub.app>
2026-05-01 20:33:34 +0200

    Should have fixed use after free now

--- src/sys/openbsd/mod.rs
+++ src/sys/openbsd/mod.rs
@@ -6,25 +6,27 @@ pub struct ScanOpenBsd;
 
 impl WlanScanner for ScanOpenBsd {
     fn scan(&mut self) -> Result<Vec<Wifi>> {
-        let networks = unsafe {
+        unsafe {
             let networks_ptr = get_networks();
             if networks_ptr.is_null() {
                 return Err(Error::ScanFailed("Unknown error occurred".to_string()));
             }
             let networks: Vec<ScanResult> = NetworkList(networks_ptr).into();
-            free_networks(networks_ptr);
-            networks
-        };
 
-        // TODO: missing fields
-        Ok(networks.iter().map(|network| {
-            Wifi {
-                mac: ConstCharArray(network.bssid).into(),
-                ssid: ConstCharArray(network.ssid).into(),
-                channel: 0,
-                signal_level: network.rssi,
-                security: Vec::new()
-            }
-        }).collect())
+            // TODO: missing fields
+            let result = networks.iter().map(|network| {
+                Wifi {
+                    mac: ConstCharArray(network.bssid).into(),
+                    ssid: ConstCharArray(network.ssid).into(),
+                    channel: 0,
+                    signal_level: network.rssi,
+                    security: Vec::new()
+                }
+            }).collect();
+
+            free_networks(networks_ptr);
+
+            Ok(result)
+        }
     }
 }
